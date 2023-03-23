3ae63826ca12c55d9d6cbd4fa449d130e1c21c6d
Pavellow <polo2b1.pav@gmail.com>
2023-03-23 13:54:00 +0100

    feat: ajout fichier connect.php

---
+++ config/connect.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once './config/db.php'
+
+
+    try{
+        $infos_db = 'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';charset=utf8';
+        $conn = new PDO($infos_db, DB_USER, DB_PWD);
+        //On définit le mode d'erreur de PDO sur Exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);        
+    }
+    
+    /*On capture les exceptions si une exception est lancée et on affiche
+     *les informations relatives à celle-ci*/
+    catch(PDOException $e){
+      echo "Erreur : " . $e->getMessage();
+    }
+    
+    $conn = null;
+?>
