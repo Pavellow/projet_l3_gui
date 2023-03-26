76a80736f427a9f0829d8a264e84790557dbf4e9
Pavellow <polo2b1.pav@gmail.com>
2023-03-27 00:39:46 +0200

    feat: redicrection vers connexion

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ if(!empty($_SESSION['pseudo'])) {
     setcookie('cookie_id_utilisateur', $_SESSION['id_utilisateur']);
     setcookie('cookie_pseudo', $_SESSION['pseudo']);
 }
+else {
+    header('Location: ./view/connexion.html');
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -17,7 +20,7 @@ if(!empty($_SESSION['pseudo'])) {
                 <img src="images/background.png" alt="texte" loading="lazy" class="img-chaus" id="img-chaus" />
                 <p id="description">Description</p>
             </div>
-            
+            <img src="https://images.stockx.com/360/Air-Jordan-3-Retro-White-Cement-Reimagined/Images/Air-Jordan-3-Retro-White-Cement-Reimagined/Lv2/img01.jpg" alt="lien pas valide" />
             <div class="boutons">
                 <button id="bouton-non" class="button-main">Non</button>
                 <button id="bouton-retour" class="button-main">Retour</button>
